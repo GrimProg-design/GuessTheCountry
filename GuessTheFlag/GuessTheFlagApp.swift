3400ef3493e2e5dcde11040d20ccd50b1499eef1
GrimProg <vavilovila01@gmail.com>
2026-01-20 15:40:55 +0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Вавилов Илья on 20/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by Вавилов Илья on 20/1/26.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
